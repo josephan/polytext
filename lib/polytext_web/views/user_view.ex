c9712d39e42f0b920d47407fd5180742ee508cf8
Joseph An <an.joseph891@gmail.com>
2017-11-21 22:58:15 -0500

    Fix user view

--- lib/polytext_web/views/user_view.ex
+++ lib/polytext_web/views/user_view.ex
@@ -1,23 +1,3 @@
-defmodule PolytextWeb.Api.DocumentView do
+defmodule PolytextWeb.UserView do
   use PolytextWeb, :view
-  alias PolytextWeb.Api.{DocumentView, SentenceView}
-
-  def render("index.json", %{documents: documents}) do
-    %{data: render_many(documents, DocumentView, "document.json")}
-  end
-
-  def render("show.json", %{document: document}) do
-    %{data: render_one(document, DocumentView, "document.json")}
-  end
-
-  def render("document.json", %{document: document}) do
-    %{id: document.id,
-      title: document.title}
-  end
-  
-  def render("document_with_sentences.json", %{document: document}) do
-    %{id: document.id,
-      title: document.title,
-      sentences: render_many(document.sentences, SentenceView, "sentence_with_translations.json")}
-  end
 end
