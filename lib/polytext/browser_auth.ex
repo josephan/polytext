c59c6f481046ea06c04f0d0fe6b639f56b7710b8
Joseph An <an.joseph891@gmail.com>
2017-12-25 20:34:24 -0500

    Add socket authentication

--- lib/polytext/browser_auth.ex
+++ lib/polytext/browser_auth.ex
@@ -10,15 +10,25 @@ defmodule Polytext.BrowserAuth do
   def call(conn, _opts) do
     user_id = get_session(conn, :user_id)
     user = user_id && Repo.get(User, user_id)
-    assign(conn, :current_user, user)
+
+    assign_user(conn, user)
   end
 
   def login(conn, user) do
     conn
-    |> assign(:current_user, user)
+    |> assign_user(user)
     |> put_session(:user_id, user.id)
     |> configure_session(renew: true)
   end
+
+  defp assign_user(conn, user) do
+    conn
+    |> assign(:current_user, user)
+    |> assign(:user_token, token(user))
+  end
+
+  defp token(nil), do: nil
+  defp token(user), do: Phoenix.Token.sign(PolytextWeb.Endpoint, "user socket", user.id)
 
   import Comeonin.Bcrypt, only: [checkpw: 2, dummy_checkpw: 0]
   def login_by_email_and_pass(conn, email, given_pass) do
